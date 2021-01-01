05603e58912297f2527e212c6a9ad2178ac1ee16
vineeths96 <vinsree1996@gmail.com>
2021-01-01 11:19:58 +0530

    Add PCA

---
+++ image_pca/pca.py
@@ -0,0 +1,18 @@
+from sklearn.decomposition import PCA
+from .load_data import load_image_patches
+from .utils import plot_image_grid
+from .parameters import *
+
+
+def natural_image_pca():
+    data = load_image_patches('./input/dataset/BSR/BSDS500/data/images/train/')
+
+    image_pca = PCA(n_components=PCA_COMPONENTS)
+    image_pca.fit(data)
+    pca_weights = []
+
+    for ind, pca_component in enumerate(image_pca.components_, 1):
+        image = pca_component.reshape(PATCH_LEN, PATCH_LEN)
+        pca_weights.append(image)
+
+    plot_image_grid(pca_weights)
